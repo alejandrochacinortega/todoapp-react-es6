814bebb3050ed9d1520bf5c2e44384fb287f390a
omar.chacin <omar.chacin@sigmagroup.no>
2016-10-12 08:09:21 +0200

    Push new items to the list

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import TodosList from './TodosList';
+import CreateTodo from './CreateTodo';
+
 
 
 const todos = [
@@ -26,9 +28,18 @@ class App extends React.Component {
     return (
       <div>
         <h1>React todos app</h1>
+        <CreateTodo createTask={this.createTask.bind(this)}/>
         <TodosList todos={this.state.todos}/>
       </div>
     );
+  }
+
+  createTask(task) {
+    this.state.todos.push({
+      task,
+      isCompleted: false
+    })
+    this.setState({todos: this.state.todos})
   }
 }
 
