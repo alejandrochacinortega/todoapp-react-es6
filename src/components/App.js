a23cfc3ecf8b7e0f14832c1481453bbd546c1602
omar.chacin <omar.chacin@sigmagroup.no>
2016-10-12 09:04:49 +0200

    save, edit and delete

--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,11 @@ class App extends React.Component {
       <div>
         <h1>React todos app</h1>
         <CreateTodo createTask={this.createTask.bind(this)}/>
-        <TodosList todos={this.state.todos} toggleTask={this.toggleTask.bind(this)}/>
+        <TodosList
+          todos={this.state.todos}
+          toggleTask={this.toggleTask.bind(this)}
+          saveTask={this.saveTask.bind(this)}
+          deleteTask={this.deleteTask.bind(this)}/>
       </div>
     );
   }
@@ -44,8 +48,21 @@ class App extends React.Component {
     this.state.todos.push({
       task,
       isCompleted: false
-    })
+    });
     this.setState({todos: this.state.todos})
+  }
+
+  saveTask(oldTask, newTask) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task == oldTask);
+    foundTodo.task = newTask;
+    this.setState({ todos: this.state.todos})
+  }
+
+  deleteTask(taskToDelete) {
+    console.log('task to delete ', taskToDelete);
+    _.remove(this.state.todos, todo => todo.task === taskToDelete);
+    this.setState({ todos: this.state.todos })
+
   }
 }
 
